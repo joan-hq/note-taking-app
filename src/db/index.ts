65186a4e235c29621e916d50431ccad775172a09
joan-huang <14166276+joan-hq@users.noreply.github.com>
2026-05-25 23:52:50 +0800

    fix: database url

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
-// import * as dotenv from 'dotenv';
 
 
 const connectionString = process.env.DATABASE_URL;
